3b33498ffa25eb2b7a10e10257326c1cf1b49ca0
briandridge <briandridge@gmail.com>
2016-11-03 12:00:40 -0600

    Q 1 and 2 initial work

--- starter-code/functions.js
+++ starter-code/functions.js
@@ -4,12 +4,33 @@ function maxOfTwoNumbers() {
 }
 
 // Question 2
-function maxOfThree() {
+function maxOfThree(one, two, three) {
+	console.log("Hello!");
+	if (one > two && one > three) {
+		return one;
+	} else if (two > one && two > three) {
+		return two;
+	} else {
+		return three;
+	}
 
 }
 
 // Question 3
-function isCharacterAVowel() {
+function isCharacterAVowel(x) {
+	if (x == "a" || x == "e" || x == "i" || x == "o" || x == "u" ||) {
+		return "True";
+	} else {
+		return "False";
+	}
+	}
+
+	var vowels = [a,e,i,o,u];
+	function checkvowels();
+		for (var i = 0; i < vowels.length; i++) {
+			vowels[i]
+		}
+	if (x === a ||) {}
 
 }
 
